46dbea157314040c4632d4301882a94c7f94449d
Alexandros Korsakov <korsakov.alexandros@gmail.com>
2023-03-27 17:41:08 +0300

    mixing @RequestParam and @PathVariable for testing

--- src/main/java/com/mycompany/propertymanagement/controller/CalculatorController.java
+++ src/main/java/com/mycompany/propertymanagement/controller/CalculatorController.java
@@ -1,16 +1,23 @@
 package com.mycompany.propertymanagement.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/calculator")
 public class CalculatorController {
 
-    @GetMapping("/add")
-    public Double add(@RequestParam("num1") Double num1, @RequestParam("num2") Double num2){
-        return num1+num2;
+    @GetMapping("/add/{num3}")
+    public Double add(@RequestParam("num1") Double num1, @RequestParam("num2") Double num2, @PathVariable("num3") Double num3){
+        return num1+num2+num3;
+    }
+
+    @GetMapping("/sub/{num1}/{num2}")
+    public Double subtract(@PathVariable("num1") Double num1, @PathVariable("num2") Double num2) {
+        if(num1>num2){
+            return num1-num2;
+        }
+        else {
+            return num2-num1;
+        }
     }
 }
--- src/main/java/com/mycompany/propertymanagement/controller/CalculatorMain.java
+++ src/main/java/com/mycompany/propertymanagement/controller/CalculatorMain.java
@@ -4,7 +4,7 @@ public class CalculatorMain {
 
     public static void main(String[] args) {
         CalculatorController cc = new CalculatorController();
-        Double result = cc.add(4.5,8.5);
+        Double result = cc.add(4.5,8.5,1.1);
         System.out.println(result);
     }
 }
