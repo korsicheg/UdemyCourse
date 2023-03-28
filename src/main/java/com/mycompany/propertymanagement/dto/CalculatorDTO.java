9e0ed03479d01ba089495cd4d438021b2e102644
Alexandros Korsakov <korsakov.alexandros@gmail.com>
2023-03-28 13:11:56 +0300

    Variables initialisation + @JsonProperty test

--- src/main/java/com/mycompany/propertymanagement/dto/CalculatorDTO.java
+++ src/main/java/com/mycompany/propertymanagement/dto/CalculatorDTO.java
@@ -1,4 +1,15 @@
 package com.mycompany.propertymanagement.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CalculatorDTO {
+    private Double num1;
+    private Double num2;
+    private Double num3;
+    @JsonProperty("num5")
+    private Double num4;
 }
