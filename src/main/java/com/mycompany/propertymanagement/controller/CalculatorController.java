0f6e70a3bce982db84435423308fd810f7f1fac7
Alexandros Korsakov <korsakov.alexandros@gmail.com>
2023-03-28 13:11:17 +0300

    Multiply method created with @RequestBody test to test POST

--- src/main/java/com/mycompany/propertymanagement/controller/CalculatorController.java
+++ src/main/java/com/mycompany/propertymanagement/controller/CalculatorController.java
@@ -1,17 +1,20 @@
 package com.mycompany.propertymanagement.controller;
 
+import com.mycompany.propertymanagement.dto.CalculatorDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/calculator")
 public class CalculatorController {
 
-    @GetMapping("/add/{num3}")
+    @GetMapping("/add/{num3}")//mix of RequestParam with PathVariable
     public Double add(@RequestParam("num1") Double num1, @RequestParam("num2") Double num2, @PathVariable("num3") Double num3){
         return num1+num2+num3;
     }
 
-    @GetMapping("/sub/{num1}/{num2}")
+    @GetMapping("/sub/{num1}/{num2}")//PathVariable test
     public Double subtract(@PathVariable("num1") Double num1, @PathVariable("num2") Double num2) {
         if(num1>num2){
             return num1-num2;
@@ -19,5 +22,11 @@ public class CalculatorController {
         else {
             return num2-num1;
         }
+    }
+
+    @PostMapping("/mul")
+    public ResponseEntity<Double> multiply(@RequestBody CalculatorDTO calculatorDTO){
+        Double result = calculatorDTO.getNum1() * calculatorDTO.getNum2() * calculatorDTO.getNum3() * calculatorDTO.getNum4();
+        return new ResponseEntity<>(result, HttpStatus.CREATED);
     }
 }
